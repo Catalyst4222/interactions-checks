99ee39f8a676435d39b1e026a3c8704a66d4f4f1
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2022-01-08 12:01:21 -0500

    feat: premade checks and custom errors

--- interactions/ext/checks/checks.py
+++ interactions/ext/checks/checks.py
@@ -1,6 +1,7 @@
 import functools
 from inspect import isawaitable, getfullargspec
-from typing import Union, Callable, Coroutine, Awaitable, TypeVar, Any, TYPE_CHECKING
+from typing import Union, Callable, Awaitable, TypeVar, Any, TYPE_CHECKING
+from pprint import pprint as print
 
 from . import errors
 
@@ -9,7 +10,7 @@ if TYPE_CHECKING:
 
     Check = Callable[[CommandContext], Union[bool, Awaitable[bool]]]
     _T = TypeVar("_T")
-    Coro = Callable[..., Coroutine[Any]]
+    Coro = Callable[..., Awaitable[Any]]
 
 
 def check(predicate: "Check") -> Callable[["Coro"], "Coro"]:
@@ -21,7 +22,9 @@ def check(predicate: "Check") -> Callable[["Coro"], "Coro"]:
     :type predicate: Callable[[CommandContext], Union[bool, Awaitable[bool]]]
     """
 
-    def decorator(coro: Callable[..., Coroutine[_T]]) -> Callable[..., Coroutine[_T]]:
+    def decorator(
+        coro: Callable[..., Awaitable["_T"]]
+    ) -> Callable[..., Awaitable["_T"]]:
 
         if getfullargspec(coro).args[0] == "self":  # To future proof classes
 
@@ -52,3 +55,83 @@ def check(predicate: "Check") -> Callable[["Coro"], "Coro"]:
         return inner
 
     return decorator
+
+
+def is_owner() -> Callable[["Coro"], "Coro"]:
+    """
+    A check that only succeeds when the user is the owner of the bot
+
+    :raise errors.NotOwner: The command user is not the bot owner
+    """
+
+    async def predicate(ctx):
+        info = await ctx.client.http.get_current_bot_information()
+        if int(ctx.author.user.id) == int(info["owner"]["id"]):
+            return True
+        raise errors.NotOwner
+
+    return check(predicate)
+
+
+def guild_only() -> Callable[["Coro"], "Coro"]:
+    """
+    A check that only succeeds when the command is ran in a guild
+
+    :raise errors.NoDMs: The command was ran in a DM
+    """
+
+    def predicate(ctx: "CommandContext"):
+        if ctx.guild_id is None:
+            raise errors.NoDMs
+        return True
+
+    return check(predicate)
+
+
+def dm_only() -> Callable[["Coro"], "Coro"]:
+    """
+    A check that only succeeds when the command is ran outside a guild
+
+    :raise errors.DMsOnly: The command was ran outside of a DM
+    """
+
+    def predicate(ctx: "CommandContext"):
+        if ctx.guild_id is not None:
+            raise errors.DMsOnly
+        return True
+
+    return check(predicate)
+
+
+# TODO Pending an actual permissions object
+# def has_permissions(**perms: dict[str, bool]) -> Callable[["Coro"], "Coro"]:
+#
+#     def predicate(ctx: "CommandContext") -> bool:
+#         print(ctx.author._json)
+#         for perm, value in perms:
+#             ...
+#         return True
+#
+#     return check(predicate)
+
+
+def is_admin(guild_only: bool = True) -> Callable[["Coro"], "Coro"]:
+    """
+    A check that only succeeds when the user is an administrator
+
+    :param bool guild_only: If the command should fail if in a dm
+    :raise errors.NoDMs: The command was ran in a DM
+    :raise errors.NotAdmin: The command was ran by a standard user
+    """
+
+    def predicate(ctx: "CommandContext"):
+        if ctx.guild_id is None:
+            if guild_only:
+                raise errors.NoDMs
+            return True
+
+        if int(ctx.author.permissions) & 8:  # 8 is the administrator permission
+            return True
+        raise errors.NotAdmin
+
+    return check(predicate)
--- interactions/ext/checks/errors.py
+++ interactions/ext/checks/errors.py
@@ -1,2 +1,40 @@
-class CheckFailure:
-    pass
+# TODO super().__init__ w/ default error messages
+
+
+class CheckFailure(Exception):
+    """Generic error to be raised when a check failed"""
+
+
+class NotOwner(CheckFailure):
+    """Raised by :func:`checks.is_owner` when the user isn't the bot owner"""
+
+
+class NoDMs(CheckFailure):
+    """Raised by :func:`checks.guild_only` when a command is ran in dms"""
+
+
+class DMsOnly(CheckFailure):
+    """Raised by :func:`checks.dm_only` when a command isn't ran in dms"""
+
+
+class MissingPermissions(CheckFailure):
+    """Raised by :func:`checks.has_permissions` when the user doesn't have the required permissions"""
+
+    def __init__(self, missing_permissions: list[str], *args) -> None:
+        self.missing_permissions: list[str] = missing_permissions
+
+        missing = [
+            perm.replace("_", " ").replace("guild", "server").title()
+            for perm in missing_permissions
+        ]
+
+        if len(missing) > 2:
+            fmt = "{}, and {}".format(", ".join(missing[:-1]), missing[-1])
+        else:
+            fmt = " and ".join(missing)
+        message = f"You are missing {fmt} permission(s) to run this command."
+        super().__init__(message, *args)
+
+
+class NotAdmin(CheckFailure):
+    """Raised by :func:`checks.has_permissions` when the user is not an administrator"""
